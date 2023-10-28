cede7f99258822e407b3206d92986727cad3ddbc
Luca Daia <87373703+LucaZeu@users.noreply.github.com>
2023-10-28 17:50:05 +0300

    te pwp monicus

--- app/src/main/java/com/example/finalfrate/MainActivity.java
+++ app/src/main/java/com/example/finalfrate/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.finalfrate;
 
+import android.os.Vibrator;
+import com.google.android.gms.maps.model.Circle;
+
+
+
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
-import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -24,15 +31,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.maps.android.SphericalUtil;
 
+import android.os.Handler;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -40,13 +49,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     FirebaseDatabase firebaseDatabase;
     private MapView mapView;
     private LatLng coordinatesToSend;
+    String description;
     private GoogleMap googleMap;
 
     private List<Marker> markers = new ArrayList<>();
 
     Button btnAlert;
 
+    private List<Circle> circles = new ArrayList<>();
+
+
+    /// handler && runnable
+    private Handler locationCheckHandler;
+    private Runnable locationCheckRunnable;
+    private static final int LOCATION_CHECK_INTERVAL = 20 * 1000; // 20 seconds in milliseconds
+
     private static final int REQUEST_LOCATION_PERMISSION = 1;
+
+
+    //for vibration
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,13 +83,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.getMapAsync(this);
 
 
+        // Initialize the handler and runnable
+        locationCheckHandler = new Handler();
+        locationCheckRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // Check if the user's location is inside any circles
+                checkUserLocation();
+                // Schedule the runnable to run again after LOCATION_CHECK_INTERVAL
+                locationCheckHandler.postDelayed(this, LOCATION_CHECK_INTERVAL);
+            }
+        };
+
+        locationCheckHandler.post(locationCheckRunnable);
+
         btnAlert = findViewById(R.id.sendToFirebaseButton);
         btnAlert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(coordinatesToSend!=null) {
-                    sendCoordinatesToFirebase(coordinatesToSend);
-                    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                    openDialog();
                 }
             }
         });
@@ -87,12 +122,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     // Create markers on the map for each saved location
                     if (myLatLng != null) {
                         LatLng latLng = new LatLng(myLatLng.latitude, myLatLng.longitude);
+                        System.out.println(myLatLng.description + " AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
                         MarkerOptions markerOptions = new MarkerOptions()
                                 .position(latLng)
-                                .title("Location")
-                                .snippet("Saved Location Description");
+                                .title(myLatLng.description)
+                                .snippet(myLatLng.description);
                         Marker newMarker = map.addMarker(markerOptions);
                         markers.add(newMarker);
+
+                        CircleOptions circleOptions = new CircleOptions()
+                                .center(latLng)  // Center of the circle
+                                .radius(100)    // Radius in meters (adjust as needed)
+                                .strokeColor(0xFFFF0000)  // Red outline color
+                                .fillColor(0x44FF0000);   // Red fill color with transparency
+
+                        Circle circle = map.addCircle(circleOptions);
+
+                        circles.add(circle);
                     }
                     System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
                 }
@@ -114,7 +160,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
-                // Clear existing markers
+
 
                 // Add a marker at the clicked location
                 MarkerOptions markerOptions = new MarkerOptions()
@@ -156,12 +202,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     // Send the coordinates to Firebase
-    private void sendCoordinatesToFirebase(LatLng latLng) {
+    private void sendCoordinatesToFirebase(LatLng latLng, String description) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("locations");
 
         LatLng cord = coordinatesToSend; // Your LatLng object
-        MyLatLong myLatLng = new MyLatLong(latLng.latitude, latLng.longitude);
+        MyLatLong myLatLng = new MyLatLong(latLng.latitude, latLng.longitude, description);
         String key = databaseReference.push().getKey();
 
         if (key != null) {
@@ -169,5 +215,95 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    private void openDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter a description");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String description = input.getText().toString();
+
+                if (!description.isEmpty()) {
+                    // Save the coordinates and description to Firebase
+                    sendCoordinatesToFirebase(coordinatesToSend, description);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    // Method to periodically check the user's location against circles
+    private void checkUserLocation() {
+        // Get the user's current location
+        if (googleMap != null) {
+            googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(@NonNull Location location) {
+                    LatLng userCurrentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    // Loop through the circles and check if the user is inside any of them
+                    for (Circle circle : circles) {
+                        if (isLocationInsideCircle(userCurrentLocation, circle)) {
+                            showAlert("Alert", "You are inside a circle.");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+
+    // Helper method to find the circle associated with a marker
+    private Circle findCircleForMarker(Marker marker) {
+        // Loop through the circles and find the one associated with the marker
+        for (Circle circle : circles) {
+            LatLng circleCenter = circle.getCenter();
+            if (circleCenter.equals(marker.getPosition())) {
+                return circle;
+            }
+        }
+        return null;
+    }
+
+    // Define a method to check if a location (LatLng) is inside a circle
+    private boolean isLocationInsideCircle(LatLng location, Circle circle) {
+        double radius = circle.getRadius();
+        LatLng center = circle.getCenter();
+
+        double distance = SphericalUtil.computeDistanceBetween(location, center);
+
+        return distance <= radius;
+    }
+
+
+    private void showAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.create().show();
+    }
     // Handle other lifecycle methods like onPause, onDestroy, etc.
 }
+
+
--- app/src/main/java/com/example/finalfrate/MyLatLong.java
+++ app/src/main/java/com/example/finalfrate/MyLatLong.java
@@ -3,14 +3,16 @@ package com.example.finalfrate;
     public class MyLatLong {
         public double latitude;
         public double longitude;
+        public String description;
 
         public MyLatLong() {
             // Default constructor is required by Firebase
         }
 
-        public MyLatLong(double latitude, double longitude) {
+        public MyLatLong(double latitude, double longitude, String description) {
             this.latitude = latitude;
             this.longitude = longitude;
+            this.description = description;
         }
     }
 
